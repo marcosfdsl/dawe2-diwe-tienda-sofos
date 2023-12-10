80014388ebb8e3db5c2e2ed9dad8c203131218cc
marcosfdsl <marcos.fernandez@live.u-tad.com>
2023-12-10 05:57:05 +0100

    ca3

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let elemento = document.querySelector('.sliding-background');
-    if (elemento) {
-        var animatedElement = document.querySelector('.sliding-background');
-        let resultado = elemento.clientHeight - window.innerHeight;
-        animatedElement.style.setProperty('--valor', `-${resultado}px`);
-    } else {
-        console.error('Elemento no encontrado con la clase "sliding-background"');
-    }
+    var animatedElement = document.querySelector('.sliding-background');
+    let resultado = elemento.clientHeight - window.innerHeight - 2000;
+    animatedElement.style.setProperty('--valor', `-${resultado}px`);
 });
