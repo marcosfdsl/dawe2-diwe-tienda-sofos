f45c7b45ec95da281a854e00fa53f4d93dfcead8
marcosfdsl <marcos.fernandez@live.u-tad.com>
2023-12-10 06:02:53 +0100

    ca3

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     let elemento = document.querySelector('.sliding-background');
     var animatedElement = document.querySelector('.sliding-background');
-    let resultado = elemento.clientHeight - window.innerHeight - 2000;
+    let resultado = elemento.clientHeight - window.innerHeight - 1000;
     animatedElement.style.setProperty('--valor', `-${resultado}px`);
 });
