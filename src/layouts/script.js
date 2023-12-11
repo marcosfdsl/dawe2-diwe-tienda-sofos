b7f81a6ef23bbc55f43b0c6c9575b2090ed79cbc
marcosfdsl <marcos.fernandez@live.u-tad.com>
2023-12-11 22:18:15 +0100

    ca8

--- src/layouts/script.js
+++ src/layouts/script.js
@@ -19,6 +19,10 @@ document.addEventListener("DOMContentLoaded", function () {
             contenedorelementos.classList.add('disappeared');
         } else if (event.target !== testc1 && !testc1.contains(event.target) && event.target !== testc2 && !testc2.contains(event.target) && event.target !== testc3 && !testc3.contains(event.target) && event.target !== testc4 && !testc4.contains(event.target)) {
             contenedorelementos.classList.remove('disappeared');
+            document.getElementById("carrito").innerHTML = "";
+            document.getElementById("carritoc2").innerHTML = "";
+            document.getElementById("carritoc3").innerHTML = "";
+            document.getElementById("carritoc4").innerHTML = "";
         }
 
         for (const key in elements) {
@@ -50,57 +54,81 @@ document.addEventListener("DOMContentLoaded", function () {
 
     setInterval(nextImage, 5000); //cada 5s
 
-// Restaurar el contador del carrito al cargar la página
-var contadorCarrito = document.getElementById('contadorCarrito');
-var storedCounter = localStorage.getItem('contadorCarrito');
-var contadorActual = parseInt(storedCounter) || 0;
-contadorCarrito.textContent = contadorActual;
-
-// Función para agregar un elemento al carrito
-function agregarAlCarrito(talla, producto) {
-    var nuevoElemento = document.createElement('div');
-    nuevoElemento.textContent = 'Añadido al carrito: ' + producto + ' ' + talla;
-
-    // Obtener el div del carrito y agregar el nuevo elemento al inicio
-    var carritoDiv = document.querySelector('#carrito');
-    carritoDiv.insertBefore(nuevoElemento, carritoDiv.firstChild);
-
-    // Obtener el contador del carrito y guardarlo en localStorage
-    contadorActual++;
+    // Restaurar el contador del carrito al cargar la página
+    var contadorCarrito = document.getElementById('contadorCarrito');
+    var storedCounter = localStorage.getItem('contadorCarrito');
+    var contadorActual = parseInt(storedCounter) || 0;
     contadorCarrito.textContent = contadorActual;
-    localStorage.setItem('contadorCarrito', contadorActual);
-}
 
-// Evento para formularioCarrito
-document.getElementById('formularioCarrito').addEventListener('submit', function (event) {
-    event.preventDefault();
-    var tallaSeleccionada = document.getElementById('talla').value;
-    var productoSeleccionado = document.getElementById('producto').value;
-    agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
-});
+    // Función para agregar un elemento al carrito
+    function agregarAlCarrito(talla, producto) {
+        document.getElementById("carrito").innerHTML = "";
+        document.getElementById("carritoc2").innerHTML = "";
+        document.getElementById("carritoc3").innerHTML = "";
+        document.getElementById("carritoc4").innerHTML = "";
 
-// Evento para formularioCarritoc2
-document.getElementById('formularioCarritoc2').addEventListener('submit', function (event) {
-    event.preventDefault();
-    var tallaSeleccionada = document.getElementById('tallac2').value;
-    var productoSeleccionado = document.getElementById('productoc2').value;
-    agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
-});
+        var nuevoElemento = document.createElement('div');
+        var nuevoElementoc2 = document.createElement('div');
+        var nuevoElementoc3 = document.createElement('div');
+        var nuevoElementoc4 = document.createElement('div');
 
-// Evento para formularioCarritoc3
-document.getElementById('formularioCarritoc3').addEventListener('submit', function (event) {
-    event.preventDefault();
-    var tallaSeleccionada = document.getElementById('tallac3').value;
-    var productoSeleccionado = document.getElementById('productoc3').value;
-    agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
-});
+        nuevoElemento.textContent = 'Añadido al carrito: ' + producto + ' ' + talla;
+        nuevoElementoc2.textContent = 'Añadido al carrito: ' + producto + ' ' + talla;
+        nuevoElementoc3.textContent = 'Añadido al carrito: ' + producto + ' ' + talla;
+        nuevoElementoc4.textContent = 'Añadido al carrito: ' + producto + ' ' + talla;
 
-// Evento para formularioCarritoc4
-document.getElementById('formularioCarritoc4').addEventListener('submit', function (event) {
-    event.preventDefault();
-    var tallaSeleccionada = document.getElementById('tallac4').value;
-    var productoSeleccionado = document.getElementById('productoc4').value;
-    agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
-});
+
+
+
+        // Obtener el div del carrito y agregar el nuevo elemento al inicio
+        var carritoDiv = document.querySelector('#carrito');
+        carritoDiv.insertBefore(nuevoElemento, carritoDiv.firstChild);
+
+        var carritoDivc2 = document.querySelector('#carritoc2');
+        carritoDivc2.insertBefore(nuevoElementoc2, carritoDivc2.firstChild);
+
+        var carritoDivc3 = document.querySelector('#carritoc3');
+        carritoDivc3.insertBefore(nuevoElementoc3, carritoDivc3.firstChild);
+
+        var carritoDivc4 = document.querySelector('#carritoc4');
+        carritoDivc4.insertBefore(nuevoElementoc4, carritoDivc4.firstChild);
+
+        // Obtener el contador del carrito y guardarlo en localStorage
+        contadorActual++;
+        contadorCarrito.textContent = contadorActual;
+        localStorage.setItem('contadorCarrito', contadorActual);
+    }
+
+    // Evento para formularioCarrito
+    document.getElementById('formularioCarrito').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var tallaSeleccionada = document.getElementById('talla').value;
+        var productoSeleccionado = document.getElementById('producto').value;
+        agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
+    });
+
+    // Evento para formularioCarritoc2
+    document.getElementById('formularioCarritoc2').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var tallaSeleccionada = document.getElementById('tallac2').value;
+        var productoSeleccionado = document.getElementById('productoc2').value;
+        agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
+    });
+
+    // Evento para formularioCarritoc3
+    document.getElementById('formularioCarritoc3').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var tallaSeleccionada = document.getElementById('tallac3').value;
+        var productoSeleccionado = document.getElementById('productoc3').value;
+        agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
+    });
+
+    // Evento para formularioCarritoc4
+    document.getElementById('formularioCarritoc4').addEventListener('submit', function (event) {
+        event.preventDefault();
+        var tallaSeleccionada = document.getElementById('tallac4').value;
+        var productoSeleccionado = document.getElementById('productoc4').value;
+        agregarAlCarrito(tallaSeleccionada, productoSeleccionado);
+    });
 
 });
